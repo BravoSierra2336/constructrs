c5e9f7b1be079266f714f52e42790c7235918141
BravoSierra2336 <sdv2336jr@gmail.com>
2025-08-11 11:41:23 -0400

    -m

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,25 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          ui: ['react-bootstrap', 'bootstrap'],
+          utils: ['axios']
+        }
+      }
+    }
+  },
+  server: {
+    port: 5173,
+    host: true
+  },
+  preview: {
+    port: 4173,
+    host: true
   }
 })
